508c501e4982731e38d8f305fd5e837dc41ead3a
branchwelder <htwiggsmith@gmail.com>
2023-05-25 17:59:51 -0700

    progggggress

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import { html, render } from "lit-html";
 import { map } from "lit-html/directives/map.js";
 import { live } from "lit-html/directives/live.js";
 
-const test2 = [["a", "b", "c"], "d"];
+const children = [["a", "b", "c"], ["d"]];
 
 function drop(e) {
   e.preventDefault();
   const data = e.dataTransfer.getData("application/split");
-  layout[e.target.id] = data;
+  state.layout[e.target.id] = data;
   syncView();
 }
 
@@ -16,16 +16,16 @@ const filmstrip = document.createElement("div");
 filmstrip.id = "filmstrip";
 document.body.appendChild(filmstrip);
 
-const container = document.createElement("div");
-container.id = "container";
-document.body.appendChild(container);
+const parentNode = document.createElement("div");
+parentNode.id = "container";
+document.body.appendChild(parentNode);
 
-const props = {
+const paneEvents = {
   ondrop: drop,
   ondragover: (e) => e.preventDefault(),
 };
 
-const root = new NestedSplitLayout(test2, container, props);
+const root = new NestedSplitLayout({ children, parentNode, paneEvents });
 
 function drag(e, data) {
   e.dataTransfer.setData("application/split", data);
@@ -33,41 +33,43 @@ function drag(e, data) {
 
 function testViews() {
   return html`${map(
-    Object.keys(state),
+    Object.keys(state.ops),
     (key) =>
       html`<div draggable="true" @dragstart=${(e) => drag(e, key)}>${key}</div>`
   )}`;
 }
 
 function view(key, val) {
-  return html`<div>
+  return html`<div class="pane-view">
     <div>${key}</div>
     <input
       .value=${live(val)}
       type="number"
       @input=${(e) => {
-        state[key].value = e.target.value;
+        state.ops[key].value = e.target.value;
         syncView();
       }} />
   </div>`;
 }
 
 const state = {
-  hello: { value: 10 },
-  world: { value: 20 },
-  split: { value: 30 },
-  pane: { value: 40 },
-  layout: { value: 50 },
+  ops: {
+    hello: { value: 10 },
+    world: { value: 20 },
+    split: { value: 30 },
+    pane: { value: 40 },
+    layout: { value: 50 },
+  },
+  layout: {},
 };
-
-const layout = {};
 
 render(testViews(), filmstrip);
 
 function syncView() {
-  Object.entries(layout).forEach(([paneID, stateField]) =>
+  console.log(state.layout);
+  Object.entries(state.layout).forEach(([paneID, stateField]) =>
     render(
-      view(stateField, state[stateField].value),
+      view(stateField, state.ops[stateField].value),
       document.getElementById(paneID)
     )
   );
--- layout.js
+++ layout.js
@@ -1,39 +1,136 @@
 import Split from "split.js";
+import { html, render } from "lit-html";
+
+function paneToolbar(split, close) {
+  return html`<div class="pane-toolbar">
+    <button @click=${() => split("vertical")}>
+      <i class="fa-solid fa-grip-lines-vertical fa-xs"></i>
+    </button>
+    <button @click=${() => split("horizontal")}>
+      <i class="fa-solid fa-grip-lines fa-xs"></i>
+    </button>
+    <button @click=${() => close()}>
+      <i class="fa-solid fa-xmark fa-xs"></i>
+    </button>
+  </div>`;
+}
 
 class Pane {
-  constructor(id, parentNode, props) {
-    this.id = id;
+  constructor({
+    children,
+    parentNode,
+    paneEvents,
+    index = 0,
+    onSplit,
+    onRemove,
+  }) {
+    this.id = children;
+    this.index = index;
 
     this.dom = document.createElement("div");
-    this.dom.id = this.id;
-    this.dom.classList.add("pane");
+    this.dom.classList.add("pane-container");
 
-    if (props) Object.assign(this.dom, props);
+    render(paneToolbar(onSplit, onRemove), this.dom);
+
+    this.pane = document.createElement("div");
+    this.pane.id = this.id;
+    this.pane.classList.add("pane");
+
+    this.dom.appendChild(this.pane);
+
+    if (paneEvents) Object.assign(this.pane, paneEvents);
 
     parentNode.appendChild(this.dom);
+  }
+
+  panes() {
+    return { [this.id]: this.dom };
   }
 }
 
 export class NestedSplitLayout {
-  constructor(children, parentNode, props, direction = "horizontal") {
+  constructor({
+    children,
+    parentNode,
+    paneEvents,
+    direction = "horizontal",
+    index = 0,
+    onSplit,
+    onRemove,
+  }) {
     this.direction = direction;
+    this.paneEvents = paneEvents;
+    this.id = index;
+    this.onSplit = onSplit;
+    this.onRemove = onRemove;
+
+    this.index = index;
+    this.currentChildIndex = 0;
 
     this.dom = document.createElement("div");
     this.dom.classList.add(direction);
     parentNode.appendChild(this.dom);
 
-    this.children = children.map((child) =>
-      Array.isArray(child)
-        ? new NestedSplitLayout(
-            child,
-            this.dom,
-            props,
-            direction == "horizontal" ? "vertical" : "horizontal"
-          )
-        : new Pane(child, this.dom, props)
-    );
+    this.children = children.map((child) => this.addChild(child));
 
     this.children.forEach((child) => this.dom.appendChild(child.dom));
-    Split(this.dom.children, { direction: this.direction });
+    this.split = this.doSplit();
+  }
+
+  doSplit() {
+    if (this.split) this.split.destroy();
+
+    return Split(this.dom.children, {
+      direction: this.direction,
+      gutterSize: 3,
+    });
+  }
+
+  addChild(child) {
+    const index = this.currentChildIndex;
+    const config = {
+      children: child,
+      index: index,
+      parentNode: this.dom,
+      paneEvents: this.paneEvents,
+      direction: this.direction == "horizontal" ? "vertical" : "horizontal",
+      onSplit: (direction) => this.splitChild(index, direction),
+      onRemove: () => this.removeChild(index),
+    };
+    this.currentChildIndex++;
+    return Array.isArray(child)
+      ? new NestedSplitLayout(config)
+      : new Pane(config);
+  }
+
+  splitChild(childIndex, direction) {
+    console.log("splitting child", childIndex, direction);
+    if (direction == this.direction) {
+      // we need to actually add the split the parent
+    } else {
+      // we add a new child
+    }
+  }
+
+  removeChild(childIndex) {
+    const removeIndex = this.children.findIndex(
+      (child) => child.index == childIndex
+    );
+
+    const removed = this.children.splice(removeIndex, 1);
+    this.dom.removeChild(removed[0].dom);
+
+    if (this.children.length == 0) {
+      this.onRemove(this.index);
+      return;
+    }
+
+    this.split = this.doSplit();
+  }
+
+  panes() {
+    return this.children.reduce((acc, cur) => {
+      return { ...acc, ...cur.panes() };
+    }, {});
   }
 }
